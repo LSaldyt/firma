7b3bde41d196b79d62c447a7a55da36ab6d16e49
LSaldyt <lucassaldyt@gmail.com>
2018-10-16 13:28:26 -0700

    Allows random search

--- grammar.py
+++ grammar.py
@@ -11,28 +11,32 @@ def atom():
     if random() > 0.5:
         return 'x'
     else:
-        return randint(-10000, 10000)
+        return randint(-10, 10)
 
 def operator():
     return choice(list(operators.keys()))
 
 grammar = {
-        'atom' : atom,
-        'operator' : operator,
-        'expression' : ['operator', 'atom', 'atom']
+    'atom' : atom,
+    'operator' : operator,
+    'expression' : ['operator', 'atom', 'atom']
 }
 
-def sample(element):
-    element = grammar[element]
+def sample(name):
+    element = grammar[name]
     if isinstance(element, list):
-        return list(map(sample, element))
+        return (name, list(map(sample, element)))
     else:
-        return element()
+        return (name, element())
 
 def generate():
     return sample('expression')
 
 def interpret(code):
+    if isinstance(code, tuple):
+        code = code[1]
+    if isinstance(code, list):
+        code = [t[1] for t in code]
     def f(x):
         try:
             if isinstance(code, list):
@@ -48,8 +52,6 @@ def interpret(code):
                 return x
             else:
                 return int(code)
-        except:
-            pass
-            #print('Code produced an error')
-        return None
+        except ZeroDivisionError:
+            return None
     return f
